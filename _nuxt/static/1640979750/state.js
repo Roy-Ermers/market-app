03fbb27433d01af019503dcd49e7f6b97ad32eb7
Roy-Ermers <Roy-Ermers@users.noreply.github.com>
2021-12-31 19:42:32 +0000

    deploy: 1a0547729a340e12eef37fdd7bc9898bd9b6f29f

---
+++ _nuxt/static/1640979750/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640979750",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fa93f4eb"}}}}(null,"\u002F"));
